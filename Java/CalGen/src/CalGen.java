bea9963210fcb578fadb217a077f3bdc34f05c8d
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2022-12-26 13:25:48 +0000

    Month method.

--- Java/CalGen/src/CalGen.java
+++ Java/CalGen/src/CalGen.java
@@ -15,7 +15,10 @@ import java.util.GregorianCalendar;
  */
 public class CalGen {
 
-    private GregorianCalendar gc = new GregorianCalendar();
+    private final GregorianCalendar gc = new GregorianCalendar();
+    private int currentMonth;
+    private int nextMonth;
+    private final boolean startOnMonday;
 
     /**
      * @param args the command line arguments
@@ -27,73 +30,68 @@ public class CalGen {
     }
 
     public CalGen() {
+        this.gc.setFirstDayOfWeek(Calendar.MONDAY);
+        this.startOnMonday = (this.gc.getFirstDayOfWeek() == Calendar.MONDAY);
+        this.gc.set(this.gc.get(Calendar.YEAR) + 1, 0, 1);
     }
 
     public void calendar() {
-        int currentDayOfWeek;
-        int currentMonth;
-        int nextMonth;
-        int currentPrintedDay;
-        int theMonths;
+        this.nextMonth = gc.get(Calendar.MONTH);
+        for (int theMonths = 0; theMonths < 12; theMonths++) {
+            this.currentMonth = this.nextMonth;
 
-        this.gc.setFirstDayOfWeek(Calendar.MONDAY);
-        boolean startOnMonday = (this.gc.getFirstDayOfWeek() == Calendar.MONDAY);
-        this.gc.set(this.gc.get(Calendar.YEAR) + 1, 0, 1);
+            this.month();
 
-        nextMonth = gc.get(Calendar.MONTH);
-        currentMonth = nextMonth;
-        for (theMonths = 0; theMonths < 12; theMonths++) {
             System.out.println();
-            System.out.println(this.getMonthText(gc.get(Calendar.MONTH)) + " " + this.gc.get(Calendar.YEAR));
-            if (startOnMonday) {
-                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
-            } else {
-                System.out.println("Sun Mon Tue Wed Thu Fri Sat");
-            }
-            while (currentMonth == nextMonth) {
-                for (currentPrintedDay = 1; currentPrintedDay < 8; currentPrintedDay++) {
-                    currentDayOfWeek = this.gc.get(Calendar.DAY_OF_WEEK);
-                    if (startOnMonday) {
-                        currentDayOfWeek--;
-                        if (currentDayOfWeek < 1) {
-                            currentDayOfWeek = 7;
-                        }
-                    }
-
-                    //System.out.println("Date = MON: " + gc.get(Calendar.MONTH) + " DOM: " + gc.get(Calendar.DAY_OF_MONTH) +
-                    //    " DOW: " + gc.get(Calendar.DAY_OF_WEEK) + " currentDayOfWeek = " + currentDayOfWeek + " currentPrintedDay = " + currentPrintedDay);
-                    if (currentPrintedDay != currentDayOfWeek) {
-                        System.out.print(" -! ");
-                    } else {
-                        if (this.gc.get(Calendar.DAY_OF_MONTH) > 9) {
-                            System.out.print(" ");
-                        } else {
-                            System.out.print("  ");
-                        }
-                        System.out.print(this.gc.get(Calendar.DAY_OF_MONTH) + " ");
-                        this.gc.add(Calendar.DAY_OF_MONTH, 1);
-                        nextMonth = this.gc.get(Calendar.MONTH);
-                    }
-                    if (currentMonth != nextMonth) {
-                        if (currentPrintedDay != 1) {
-                            currentPrintedDay++;
-                            while (currentPrintedDay < 8) {
-                                System.out.print(" -* ");
-                                currentPrintedDay++;
-                            }
-                        }
-                    }
-                }
-                System.out.println();
-            }
-            currentMonth = nextMonth;
-
-            //System.out.println("Date = " + gc.toString()); 
         }
     }
 
     private void month() {
+        int currentDayOfWeek;
+        int currentPrintedDay;
 
+        System.out.println(this.getMonthText(gc.get(Calendar.MONTH)) + " " + this.gc.get(Calendar.YEAR));
+        if (this.startOnMonday) {
+            System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        } else {
+            System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+        }
+        while (this.currentMonth == this.nextMonth) {
+            for (currentPrintedDay = 1; currentPrintedDay < 8; currentPrintedDay++) {
+                currentDayOfWeek = this.gc.get(Calendar.DAY_OF_WEEK);
+                if (this.startOnMonday) {
+                    currentDayOfWeek--;
+                    if (currentDayOfWeek < 1) {
+                        currentDayOfWeek = 7;
+                    }
+                }
+
+                //System.out.println("Date = MON: " + gc.get(Calendar.MONTH) + " DOM: " + gc.get(Calendar.DAY_OF_MONTH) +
+                //    " DOW: " + gc.get(Calendar.DAY_OF_WEEK) + " currentDayOfWeek = " + currentDayOfWeek + " currentPrintedDay = " + currentPrintedDay);
+                if (currentPrintedDay != currentDayOfWeek) {
+                    System.out.print(" -! ");
+                } else {
+                    if (this.gc.get(Calendar.DAY_OF_MONTH) > 9) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                    System.out.print(this.gc.get(Calendar.DAY_OF_MONTH) + " ");
+                    this.gc.add(Calendar.DAY_OF_MONTH, 1);
+                    this.nextMonth = this.gc.get(Calendar.MONTH);
+                }
+                if (this.currentMonth != this.nextMonth) {
+                    if (currentPrintedDay != 1) {
+                        currentPrintedDay++;
+                        while (currentPrintedDay < 8) {
+                            System.out.print(" -* ");
+                            currentPrintedDay++;
+                        }
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 
     private String getMonthText(int theMonth) {
