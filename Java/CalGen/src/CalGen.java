94eefaf1ab3860923503a8ddb43a16e034ee10a7
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2022-12-26 13:17:21 +0000

    Getting towards month method.

--- Java/CalGen/src/CalGen.java
+++ Java/CalGen/src/CalGen.java
@@ -10,19 +10,19 @@ import java.util.GregorianCalendar;
 
 /**
  * CalGen class.
+ *
  * @author G J Barnard
  */
 public class CalGen {
 
     private GregorianCalendar gc = new GregorianCalendar();
 
-        
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         CalGen us = new CalGen();
-        
+
         us.calendar();
     }
 
@@ -34,83 +34,98 @@ public class CalGen {
         int currentMonth;
         int nextMonth;
         int currentPrintedDay;
-        int theMonths = 0;
-    
+        int theMonths;
+
         this.gc.setFirstDayOfWeek(Calendar.MONDAY);
         boolean startOnMonday = (this.gc.getFirstDayOfWeek() == Calendar.MONDAY);
         this.gc.set(this.gc.get(Calendar.YEAR) + 1, 0, 1);
 
         nextMonth = gc.get(Calendar.MONTH);
-        currentMonth = nextMonth - 1;
-        while (theMonths <= 12) {
-            for (currentPrintedDay = 1; currentPrintedDay < 8; currentPrintedDay++) {
-                if (currentMonth != nextMonth) {
-                    if (currentPrintedDay != 1) {
-                        while (currentPrintedDay < 8) {
-                            System.out.print(" -- ");
-                            currentPrintedDay++;
-                        }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    System.out.println(this.getMonthText(gc.get(Calendar.MONTH)) + " " + this.gc.get(Calendar.YEAR));
-                    if (startOnMonday) {
-                        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
-                    } else {
-                        System.out.println("Sun Mon Tue Wed Thu Fri Sat");
-                    }
-                    currentPrintedDay = 1;
-                    currentMonth = nextMonth;
-                    theMonths++;
-                }
-
-                currentDayOfWeek = this.gc.get(Calendar.DAY_OF_WEEK);
-                if (startOnMonday) {
-                    currentDayOfWeek--;
-                    if (currentDayOfWeek < 1) {
-                        currentDayOfWeek = 7;
-                    }
-                }
-
-                //System.out.println("Date = MON: " + gc.get(Calendar.MONTH) + " DOM: " + gc.get(Calendar.DAY_OF_MONTH) +
-                //    " DOW: " + gc.get(Calendar.DAY_OF_WEEK) + " currentDayOfWeek = " + currentDayOfWeek + " currentPrintedDay = " + currentPrintedDay);
-
-                if (currentPrintedDay != currentDayOfWeek) {
-                    System.out.print(" -- ");
-                } else {
-                    if (this.gc.get(Calendar.DAY_OF_MONTH) > 9) {
-                        System.out.print(" ");
-                    } else {
-                        System.out.print("  ");
-                    }
-                    System.out.print(this.gc.get(Calendar.DAY_OF_MONTH) + " ");
-                    this.gc.add(Calendar.DAY_OF_MONTH, 1);
-                    nextMonth = this.gc.get(Calendar.MONTH);
-                }
-            }
+        currentMonth = nextMonth;
+        for (theMonths = 0; theMonths < 12; theMonths++) {
             System.out.println();
+            System.out.println(this.getMonthText(gc.get(Calendar.MONTH)) + " " + this.gc.get(Calendar.YEAR));
+            if (startOnMonday) {
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+            } else {
+                System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+            }
+            while (currentMonth == nextMonth) {
+                for (currentPrintedDay = 1; currentPrintedDay < 8; currentPrintedDay++) {
+                    currentDayOfWeek = this.gc.get(Calendar.DAY_OF_WEEK);
+                    if (startOnMonday) {
+                        currentDayOfWeek--;
+                        if (currentDayOfWeek < 1) {
+                            currentDayOfWeek = 7;
+                        }
+                    }
+
+                    //System.out.println("Date = MON: " + gc.get(Calendar.MONTH) + " DOM: " + gc.get(Calendar.DAY_OF_MONTH) +
+                    //    " DOW: " + gc.get(Calendar.DAY_OF_WEEK) + " currentDayOfWeek = " + currentDayOfWeek + " currentPrintedDay = " + currentPrintedDay);
+                    if (currentPrintedDay != currentDayOfWeek) {
+                        System.out.print(" -! ");
+                    } else {
+                        if (this.gc.get(Calendar.DAY_OF_MONTH) > 9) {
+                            System.out.print(" ");
+                        } else {
+                            System.out.print("  ");
+                        }
+                        System.out.print(this.gc.get(Calendar.DAY_OF_MONTH) + " ");
+                        this.gc.add(Calendar.DAY_OF_MONTH, 1);
+                        nextMonth = this.gc.get(Calendar.MONTH);
+                    }
+                    if (currentMonth != nextMonth) {
+                        if (currentPrintedDay != 1) {
+                            currentPrintedDay++;
+                            while (currentPrintedDay < 8) {
+                                System.out.print(" -* ");
+                                currentPrintedDay++;
+                            }
+                        }
+                    }
+                }
+                System.out.println();
+            }
+            currentMonth = nextMonth;
 
             //System.out.println("Date = " + gc.toString()); 
-        }        
+        }
     }
-    
-    private String getMonthText(int dMonth) {
+
+    private void month() {
+
+    }
+
+    private String getMonthText(int theMonth) {
         String retr;
 
-        retr = switch (dMonth) {
-            case Calendar.JANUARY -> "January";
-            case Calendar.FEBRUARY -> "February";
-            case Calendar.MARCH -> "March";
-            case Calendar.APRIL -> "April";
-            case Calendar.MAY -> "May";
-            case Calendar.JUNE -> "June";
-            case Calendar.JULY -> "July";
-            case Calendar.AUGUST -> "August";
-            case Calendar.SEPTEMBER -> "September";
-            case Calendar.OCTOBER -> "October";
-            case Calendar.NOVEMBER -> "November";
-            case Calendar.DECEMBER -> "December";
-            default -> "Unknown";
+        retr = switch (theMonth) {
+            case Calendar.JANUARY ->
+                "January";
+            case Calendar.FEBRUARY ->
+                "February";
+            case Calendar.MARCH ->
+                "March";
+            case Calendar.APRIL ->
+                "April";
+            case Calendar.MAY ->
+                "May";
+            case Calendar.JUNE ->
+                "June";
+            case Calendar.JULY ->
+                "July";
+            case Calendar.AUGUST ->
+                "August";
+            case Calendar.SEPTEMBER ->
+                "September";
+            case Calendar.OCTOBER ->
+                "October";
+            case Calendar.NOVEMBER ->
+                "November";
+            case Calendar.DECEMBER ->
+                "December";
+            default ->
+                "Unknown";
         };
 
         return retr;
