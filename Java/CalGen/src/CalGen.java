52cbc89bd81d96b866f46d6e71afe33f128ee961
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2022-12-30 13:13:40 +0000

    Month template.

--- Java/CalGen/src/CalGen.java
+++ Java/CalGen/src/CalGen.java
@@ -18,104 +18,40 @@ import java.io.IOException;
  */
 public class CalGen {
 
+    // Calendar attributes.
     private final GregorianCalendar gc = new GregorianCalendar();
     private int currentMonth;
     private int nextMonth;
     private final boolean startOnMonday;
     private final int theYear;
 
-    // Temp template markup.
-    private final String calMarkup = "<!doctype html><html><head>"
-            + "<style type=\"text/css\">"
-            + ".cal-row {display: flex; flex-wrap: wrap; justify-content: space-evenly; margin: 10px; }"
-            + ".cal-row.nowrap {flex-wrap: nowrap; }"
-            + ".cal-1 { flex-basis: 100%; }"
-            + ".cal-4 { flex-basis: 25%; }"
-            + ".cal-7 { flex-basis: 14.28%; margin-left: 2px; margin-right: 2px; }"
-            + ".cal-7:first-child { margin-left: 0; }"
-            + ".cal-7:first-child { margin-right: 0; }"
-            + ".cal-img { max-width: 100%; height: auto; }"
-            + ".monthdayname, .monthday { text-align: end; }"
-            + "h1, .monthtitle { text-align: center; }"
-            + "* { font-family: sans-serif; }"
-            + "</style>"
-            + "<title>{{calendartitle}}</title>"
-            + "</head><body>"
-            + "<h1>{{calendartitle}}</h1>"
-            + "<div class=\"cal-row\">"
-            + "{{jan-Jan_760D_3389_sRGB.webp&Female mallard duck on ice}}"
-            + "{{feb-Feb_760D_0509_sRGB.webp&Pigeon looking at the camera standing on a fence}}"
-            + "{{mar-Mar_760D_5005_sRGB.webp&Cygnet in the sun}}"
-            + "{{apr-Apr_760D_8255_sRGB.webp&Mallard duckling}}"
-            + "{{may-May_760D_4222_sRGB.webp&Midland Pullman Train, Class 43 version}}"
-            + "{{jun-Jun_760D_4905_sRGB.webp&Beer beach, Devon}}"
-            + "{{jul-Jul_760D_4809_sRGB.webp&Moorhen}}"
-            + "{{aug-Aug_760D_6863_sRGB.webp&Male mallard duck}}"
-            + "{{sep-Sep_760D_4164_sRGB.webp&Great Western Railway Class 43, 43198, Driver Stan Martin 25th June 1950 - 6th November 2004}}"
-            + "{{oct-Oct_760D_0803_sRGB.webp&White swan looking at its reflection in the water}}"
-            + "{{nov-Nov_760D_3724_sRGB.webp&Chaffinch on a fence}}"
-            + "{{dec-Dec_760D_3366_sRGB.webp&Robin looking skywards}}"
-            + "</div>"
-            + "</body></html>";
+    // Template attributes.
     private char[] calendarTemplate;
-
-    private final String monthMarkup = "<div class=\"cal-4 month\">"
-            + "<div class=\"cal-row nowrap monthheader\">"
-            + "<div class=\"cal-1 monthtitle\">{{monthtitle}}</div>"
-            + "</div>"
-            + "<div class=\"cal-row monthimagewrapper\">"
-            + "<img class=\"cal-1 cal-img monthimage\" src=\"{{monthimage}}\" alt=\"{{monthimagedescription}}\">"
-            + "</div>"
-            + "<div class=\"cal-row nowrap monthdaynames\">"
-            + "{{monthdaynames-<div class=\"cal-7 monthdayname\">*</div>}}"
-            + "</div>"
-            + "{{monthweek-<div class=\"cal-row nowrap monthweek\">!</div>&<div class=\"cal-7 monthday\">*</div>}}"
-            + "</div>";
     private char[] monthTemplate = null;
-    
+
     private final char[] mpre = {'{', '{'};
     private final char[] mpost = {'}', '}'};
     private final FileOutputStream mout;
     private final StringBuffer markupOut = new StringBuffer();
 
     /**
-     * @param args the command line arguments
+     * @param args the command line arguments - not used.
      * @throws java.io.FileNotFoundException
+     * @throws java.io.IOException
      */
     public static void main(String args[]) throws FileNotFoundException, IOException {
         CalGen us = new CalGen();
 
         us.calendar();
         us.calendarTemplate();
-
-        //us.fileTest();
     }
 
     public CalGen() throws FileNotFoundException {
-        this.gc.setFirstDayOfWeek(Calendar.MONDAY);
+        this.gc.setFirstDayOfWeek(Calendar.MONDAY); // Change to SUNDAY if wished.
         this.startOnMonday = (this.gc.getFirstDayOfWeek() == Calendar.MONDAY);
 
         this.theYear = this.gc.get(Calendar.YEAR) + 1;
         this.mout = new FileOutputStream("./calm.html");
-    }
-
-    public String fileTest() throws FileNotFoundException, IOException {
-        // Ref: https://stackoverflow.com/questions/21980090/javas-randomaccessfile-eofexception
-        java.io.File file = new java.io.File("CalendarTemplate.txt");
-        System.out.println(file.length());
-        
-        char[] buffer = new char[(int) file.length()];
-        
-        java.io.FileInputStream fin = new java.io.FileInputStream(file);
-        java.io.InputStreamReader fr = new java.io.InputStreamReader(fin, "UTF-8");
-        fr.read(buffer);
-        StringBuilder sb = new StringBuilder();
-        
-        sb.append(buffer);
-        
-        System.out.println(sb.toString());
-        
-        return sb.toString();
     }
 
     public void calendar() throws IOException, FileNotFoundException {
@@ -220,12 +156,11 @@ public class CalGen {
         System.out.print(day);
     }
 
-    public void calendarTemplate() throws IOException {
+    public void calendarTemplate() throws FileNotFoundException, IOException {
         // Reset.
         this.gc.set(theYear, 0, 1);
         try (this.mout) {
-            //this.calendarTemplate = this.calMarkup.toCharArray();
-            this.calendarTemplate = this.fileTest().toCharArray();
+            this.loadTemplates();
             int currentIndex = 0;
 
             while (currentIndex < this.calendarTemplate.length) {
@@ -242,6 +177,47 @@ public class CalGen {
 
             this.mout.write(this.markupOut.toString().getBytes());
         }
+    }
+
+    /**
+     * Load the templates.
+     *
+     * Ref: https://stackoverflow.com/questions/21980090/javas-randomaccessfile-eofexception
+     *
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    private void loadTemplates() throws FileNotFoundException, IOException {
+        // Calendar template.
+        java.io.File templateFile = new java.io.File("CalendarTemplate.txt");
+
+        char[] buffer = new char[(int) templateFile.length()];
+
+        java.io.FileInputStream fin = new java.io.FileInputStream(templateFile);
+        java.io.InputStreamReader isr = new java.io.InputStreamReader(fin, "UTF-8");
+
+        isr.read(buffer);
+        isr.close();
+
+        StringBuilder sb = new StringBuilder((int) templateFile.length());
+        sb.append(buffer);
+
+        this.calendarTemplate = sb.toString().toCharArray();
+
+        // Month template.
+        templateFile = new java.io.File("MonthTemplate.txt");
+        buffer = new char[(int) templateFile.length()];
+
+        fin = new java.io.FileInputStream(templateFile);
+        isr = new java.io.InputStreamReader(fin, "UTF-8");
+
+        isr.read(buffer);
+        isr.close();
+
+        sb = new StringBuilder((int) templateFile.length());
+        sb.append(buffer);
+
+        this.monthTemplate = sb.toString().toCharArray();
     }
 
     private int processCalendarToken(int currentIndex) {
@@ -271,7 +247,7 @@ public class CalGen {
             // We have data.
             data = token.substring(dataIndex + 1, token.length());
             token = token.substring(0, dataIndex);
-            
+
             int ampIndex = data.indexOf('&');
             if (ampIndex != 1) {
                 dataExtra = data.substring(ampIndex + 1, data.length());
@@ -279,48 +255,22 @@ public class CalGen {
             }
         }
 
+        // Rule switch, Java 12 - https://blogs.oracle.com/javamagazine/post/new-switch-expressions-in-java-12
         switch (token) {
-            case "calendartitle":
-                this.markupOut.append(this.gc.get(Calendar.YEAR)).append(" Calendar");
-                break;
-            case "jan":
-                this.monthTemplate(0, data, dataExtra);
-                break;
-            case "feb":
-                this.monthTemplate(1, data, dataExtra);
-                break;
-            case "mar":
-                this.monthTemplate(2, data, dataExtra);
-                break;
-            case "apr":
-                this.monthTemplate(3, data, dataExtra);
-                break;
-            case "may":
-                this.monthTemplate(4, data, dataExtra);
-                break;
-            case "jun":
-                this.monthTemplate(5, data, dataExtra);
-                break;
-            case "jul":
-                this.monthTemplate(6, data, dataExtra);
-                break;
-            case "aug":
-                this.monthTemplate(7, data, dataExtra);
-                break;
-            case "sep":
-                this.monthTemplate(8, data, dataExtra);
-                break;
-            case "oct":
-                this.monthTemplate(9, data, dataExtra);
-                break;
-            case "nov":
-                this.monthTemplate(10, data, dataExtra);
-                break;
-            case "dec":
-                this.monthTemplate(11, data, dataExtra);
-                break;
-            default:
-                this.markupOut.append("<p>Calendar error!</p>");
+            case "calendartitle" -> this.markupOut.append(this.gc.get(Calendar.YEAR)).append(" Calendar");
+            case "jan" -> this.monthTemplate(0, data, dataExtra);
+            case "feb" -> this.monthTemplate(1, data, dataExtra);
+            case "mar" -> this.monthTemplate(2, data, dataExtra);
+            case "apr" -> this.monthTemplate(3, data, dataExtra);
+            case "may" -> this.monthTemplate(4, data, dataExtra);
+            case "jun" -> this.monthTemplate(5, data, dataExtra);
+            case "jul" -> this.monthTemplate(6, data, dataExtra);
+            case "aug" -> this.monthTemplate(7, data, dataExtra);
+            case "sep" -> this.monthTemplate(8, data, dataExtra);
+            case "oct" -> this.monthTemplate(9, data, dataExtra);
+            case "nov" -> this.monthTemplate(10, data, dataExtra);
+            case "dec" -> this.monthTemplate(11, data, dataExtra);
+            default -> this.markupOut.append("<p>Calendar error!</p>");
         }
     }
 
@@ -329,22 +279,19 @@ public class CalGen {
         this.nextMonth = theMonth;
         this.currentMonth = this.nextMonth;
 
-        if (this.monthTemplate == null) {
-            this.monthTemplate = this.monthMarkup.toCharArray();
-        }
-            int currentIndex = 0;
+        int currentIndex = 0;
 
-            while (currentIndex < this.monthTemplate.length) {
-                if ((this.monthTemplate[currentIndex] == this.mpre[0]) && (this.monthTemplate[currentIndex + 1]) == this.mpre[1]) {
-                    // Start token.
-                    currentIndex = currentIndex + 2;
-                    currentIndex = this.processMonthToken(currentIndex, imageName, imageDescription);
-                } else {
-                    // Pass through.
-                    this.markupOut.append(this.monthTemplate[currentIndex]);
-                    currentIndex++;
-                }
+        while (currentIndex < this.monthTemplate.length) {
+            if ((this.monthTemplate[currentIndex] == this.mpre[0]) && (this.monthTemplate[currentIndex + 1]) == this.mpre[1]) {
+                // Start token.
+                currentIndex = currentIndex + 2;
+                currentIndex = this.processMonthToken(currentIndex, imageName, imageDescription);
+            } else {
+                // Pass through.
+                this.markupOut.append(this.monthTemplate[currentIndex]);
+                currentIndex++;
             }
+        }
     }
 
     private int processMonthToken(int currentIndex, String imageName, String imageDescription) {
@@ -374,30 +321,18 @@ public class CalGen {
             data = token.substring(dataIndex + 1, token.length());
             token = token.substring(0, dataIndex);
         }
-        
+
         switch (token) {
-            case "monthtitle":
-                this.markupOut.append(this.getMonthText(gc.get(Calendar.MONTH)));
-                break;
-            case "monthdaynames":
-                this.monthDayNames(data);
-                break;
-            case "monthweek":
-                this.monthWeek(data);
-                break;
-            case "monthimage":
-                this.monthImage(imageName);
-                break;
-            case "monthimagedescription":
-                this.monthImage(imageDescription);
-                break;
-            default:
-                this.markupOut.append("<p>Month error!</p>");
+            case "monthtitle" -> this.markupOut.append(this.getMonthText(gc.get(Calendar.MONTH)));
+            case "monthdaynames" -> this.monthDayNames(data);
+            case "monthweek" -> this.monthWeek(data);
+            case "monthimage" -> this.monthImage(imageName);
+            case "monthimagedescription" -> this.monthImage(imageDescription);
+            default -> this.markupOut.append("<p>Month error!</p>");
         }
     }
 
     private void monthDayNames(String data) {
-        // "{{monthdaynames-<div class=\"cal-7\">*</div>}}"
         int starIndex = data.indexOf('*');
         String pre = data.substring(0, starIndex);
         String post = data.substring(starIndex + 1, data.length());
@@ -420,7 +355,7 @@ public class CalGen {
             this.monthDay("Sat", pre, post);
         }
     }
-    
+
     private void monthDay(Integer day, String pre, String post) {
         this.monthDay(day.toString(), pre, post);
     }
@@ -428,9 +363,8 @@ public class CalGen {
     private void monthDay(String day, String pre, String post) {
         this.markupOut.append(pre).append(day).append(post);
     }
-    
+
     private void monthWeek(String data) {
-        // "{{monthweek-<div class=\"cal-row nowrap monthweek\">!</div>&<div class=\"cal-7 monthday\">*</div>}}";
         int exlamationIndex = data.indexOf('!');
         int ampIndex = data.indexOf('&');
         int starIndex = data.indexOf('*');
@@ -483,37 +417,24 @@ public class CalGen {
             this.markupOut.append(text);
         }
     }
-    
+
     private String getMonthText(int theMonth) {
         String retr;
 
         retr = switch (theMonth) {
-            case Calendar.JANUARY ->
-                "January";
-            case Calendar.FEBRUARY ->
-                "February";
-            case Calendar.MARCH ->
-                "March";
-            case Calendar.APRIL ->
-                "April";
-            case Calendar.MAY ->
-                "May";
-            case Calendar.JUNE ->
-                "June";
-            case Calendar.JULY ->
-                "July";
-            case Calendar.AUGUST ->
-                "August";
-            case Calendar.SEPTEMBER ->
-                "September";
-            case Calendar.OCTOBER ->
-                "October";
-            case Calendar.NOVEMBER ->
-                "November";
-            case Calendar.DECEMBER ->
-                "December";
-            default ->
-                "Unknown";
+            case Calendar.JANUARY -> "January";
+            case Calendar.FEBRUARY -> "February";
+            case Calendar.MARCH -> "March";
+            case Calendar.APRIL -> "April";
+            case Calendar.MAY -> "May";
+            case Calendar.JUNE -> "June";
+            case Calendar.JULY -> "July";
+            case Calendar.AUGUST -> "August";
+            case Calendar.SEPTEMBER -> "September";
+            case Calendar.OCTOBER -> "October";
+            case Calendar.NOVEMBER -> "November";
+            case Calendar.DECEMBER -> "December";
+            default -> "Unknown";
         };
 
         return retr;
