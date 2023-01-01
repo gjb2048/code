72f226db02052d227d215053ff82153e5c67b450
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2023-01-01 14:35:10 +0000

    Licence tidy.

--- Java/CalGen/src/CalGen.java
+++ Java/CalGen/src/CalGen.java
@@ -5,7 +5,21 @@
  *
  * Associated eLearningWorld post: TBC.
  *
- * @copyright  2022 G J Barnard.
+ * Copyright (C) 2022 G J Barnard.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
  */
 
@@ -21,7 +35,7 @@ import java.util.LinkedList;
 /**
  * CalGen class.
  *
- * @author G J Barnard.
+ * @copyright 2022 G J Barnard.
  */
 public class CalGen {
 
